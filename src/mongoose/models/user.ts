22fc10e8b632901583766ad1f36fbf47ce3e6445
Davidooff <david.ter.18@gmail.com>
2023-05-05 23:02:51 +0300

    Types error fix

--- src/mongoose/models/user.ts
+++ src/mongoose/models/user.ts
@@ -37,105 +37,96 @@ interface IUserMethods {
   ): Promise<[libraryElement]>;
 }
 
+interface UserModel extends Model<IUser, {}, IUserMethods> {
+  auth(login: string, password: string, ip: string): Promise<LoginResponse>;
+  register(login: string, password: string, ip: string): Promise<LoginResponse>;
+}
+
 // Create a new Model type that knows about IUserMethods...
-type UserModel = Model<IUser, {}, IUserMethods>;
 
 // And a schema that knows about IUserMethods
 
-const userSchema = new Schema<IUser, UserModel, IUserMethods>(
-  {
-    _id: String,
-    password: String,
-    library: [
-      {
-        originalName: String,
-        platform: String,
-        link: String,
-      },
-    ],
-    date: { type: Date, default: Date.now },
-  },
-  {
-    statics: {
-      async auth(
-        login: string,
-        password: string,
-        ip: string
-      ): Promise<LoginResponse> {
-        let user = this.findOne({ _id: login }).lean();
-        let refreshToken = jwtService.sign({ login, ip }, "7d");
-        let accessToken = jwtService.sign({ login }, "10m");
-        let data = await Promise.all([user, refreshToken, accessToken]);
-
-        if (data[0]?.password && compareSync(password, data[0].password)) {
-          return {
-            refreshToken: data[1],
-            accessToken: data[2],
-            success: true,
-          };
-        } else {
-          return {
-            success: false,
-          };
-        }
-      },
-      async register(
-        login: string,
-        password: string,
-        ip: string
-      ): Promise<LoginResponse> {
-        let user = this.findOne({ _id: login }).lean();
-        let refreshToken = jwtService.sign({ login, ip }, "7d");
-        let accessToken = jwtService.sign({ login }, "10m");
-        let password_hash = hashPassword(password);
-
-        if (await user) {
-          return {
-            success: false,
-            msg: "User already exists",
-          };
-        } else {
-          return this.create({
-            _id: login,
-            password: await password_hash,
-          })
-            .then(async () => {
-              return {
-                success: true,
-                refreshToken: await refreshToken,
-                accessToken: await accessToken,
-              };
-            })
-            .catch(async (err) => {
-              return {
-                success: false,
-                msg: err,
-              };
-            })
-            .finally(() => {
-              return {
-                success: false,
-                msg: "Something went wrong",
-              };
-            });
-        }
-      },
+const userSchema = new Schema<IUser, UserModel, IUserMethods>({
+  _id: String,
+  password: String,
+  library: [
+    {
+      originalName: String,
+      platform: String,
+      link: String,
     },
-    // methods: {
-    // async moveInLibrary(id, direction) {
-    //   let index = this.library.findIndex((e) => e._id == id);
-    //   let newIndex = direction;
-    //   if (newIndex < 0) {
-    //     newIndex = 0;
-    //   } else if (newIndex > this.library.length - 1) {
-    //     newIndex = this.library.length - 1;
-    //   }
-    //   let tmp = this.library[index];
-    //   this.library[index] = this.library[newIndex];
-    //   this.library[newIndex] = tmp;
-    //   return this.save();
-    // },
-    // },
+  ],
+  date: { type: Date, default: Date.now },
+});
+
+userSchema.static(
+  "auth",
+  async function auth(
+    login: string,
+    password: string,
+    ip: string
+  ): Promise<LoginResponse> {
+    let user = this.findOne({ _id: login }).lean();
+    let refreshToken = jwtService.sign({ login, ip }, "7d");
+    let accessToken = jwtService.sign({ login }, "10m");
+    let data = await Promise.all([user, refreshToken, accessToken]);
+
+    if (data[0]?.password && compareSync(password, data[0].password)) {
+      return {
+        refreshToken: data[1],
+        accessToken: data[2],
+        success: true,
+      };
+    } else {
+      return {
+        success: false,
+      };
+    }
+  }
+);
+
+userSchema.static(
+  "register",
+  async function register(
+    login: string,
+    password: string,
+    ip: string
+  ): Promise<LoginResponse> {
+    let user = this.findOne({ _id: login }).lean();
+    let refreshToken = jwtService.sign({ login, ip }, "7d");
+    let accessToken = jwtService.sign({ login }, "10m");
+    let password_hash = hashPassword(password);
+
+    if (await user) {
+      return {
+        success: false,
+        msg: "User already exists",
+      };
+    } else {
+      return this.create({
+        _id: login,
+        password: await password_hash,
+      })
+        .then(async () => {
+          return {
+            success: true,
+            refreshToken: await refreshToken,
+            accessToken: await accessToken,
+          };
+        })
+        .catch(async (err) => {
+          return {
+            success: false,
+            msg: err,
+          };
+        })
+        .finally(() => {
+          return {
+            success: false,
+            msg: "Something went wrong",
+          };
+        });
+    }
   }
 );
 
@@ -188,4 +179,4 @@ userSchema.method("getLibrary", async function getLibrary(start?, end?) {
 
 // const userModel = mongoose.model("users", userSchema);
 
-export default mongoose.model("users", userSchema);
+export default mongoose.model<IUser, UserModel>("users", userSchema);
