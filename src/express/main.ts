d4ce86e6f6088b986e9fa2730246c50391854b0b
Davidooff <david.ter.18@gmail.com>
2023-05-06 23:53:15 +0300

    cors to socket

--- src/express/main.ts
+++ src/express/main.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import http from "http";
-import Socket from "../socket/main";
 import { hello } from "./routes/hello";
 import { login, register, checkRefreshToken } from "./routes/auth";
 import {
@@ -33,7 +32,7 @@ class App {
   constructor() {
     this.app = express();
     this.server = http.createServer(this.app);
-    global.io = new Socket(this.server).io;
+    global.io = require("socket.io")(this.server, corsOptions).io;
     this.config();
     this.routs();
   }
